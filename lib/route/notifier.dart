07361429c8e1a7c9a0eb2bc9e63f274001386f28
literati <jhjung@builton.co.kr>
2023-11-07 09:13:18 +0900

    CHORE: add user id

--- lib/route/notifier.dart
+++ lib/route/notifier.dart
@@ -3,11 +3,17 @@ import 'package:united_market/route/route.dart';
 
 class PageNotifier extends ChangeNotifier {
   String _pageName = PageNames.home.name;
+  int? _userId;
 
   get pageName => _pageName;
+  get userId => _userId;
 
   changePage({required String page}) {
     _pageName = page;
     notifyListeners();
+  }
+
+  setUserId(int id) {
+    _userId = id;
   }
 }
